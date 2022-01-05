b0b38cdb42c67ff57cf0530a83ef1866a069d480
JYoonJong <dbswhd493@naver.com>
2022-01-05 22:24:58 +0900

    점프 투 자바 String

---
+++ src/String1.java
@@ -0,0 +1,30 @@
+
+public class String1 {
+
+	public static void main(String[] args) {
+		String a = "Hello Java";
+		System.out.println(a.indexOf("Java")); // 6 Ãâ·Â	
+		
+		System.out.println(a.contains("Java"));
+		
+		System.out.println(a.charAt(6));
+		
+		System.out.println(a.replaceAll("Hello", "Bye"));
+		
+		System.out.println(a.substring(0,5));
+		
+		System.out.println(a.toUpperCase());
+		
+		String b = "a:b:c:d";
+		System.out.println(b.split(":"));
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
